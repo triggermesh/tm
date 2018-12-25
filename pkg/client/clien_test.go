1826c289cc0c9eff275c4bf3a41bcd7a6d8b6f06
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-12-25 15:06:22 +0600

    #9 Add test for client package

---
+++ pkg/client/clien_test.go
@@ -0,0 +1,60 @@
+/*
+Copyright (c) 2018 TriggerMesh, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestUsername(t *testing.T) {
+	c := []byte(`{"apiVersion":"v1","clusters":[{"cluster":{"certificate-authority-data":"==","server":""},"name":"test"}],"contexts":[{"context":{"cluster":"triggermesh","namespace":"testnamespace","user":"testuser"},"name":"default-context"}],"current-context":"default-context","kind":"Config","preferences":{},"users":[{"name":"testuser","user":{"token":""}}]}`)
+	d := []byte(`{"apiVersion":"v1","clusters":[{"cluster":{"certificate-authority-data":"==","server":""},"name":"test"}],"contexts":[{"context":{"cluster":"test","namespace":"testnamespace","user":"testuser"},"name":"default-context"}],"current-context":"default-context","kind":"Config","preferences":{},"users":[{"name":"testuser","user":{"token":""}}]}`)
+
+	_ = ioutil.WriteFile("config.json", c, 0644)
+	_ = ioutil.WriteFile("default.json", d, 0644)
+
+	testCases := []struct {
+		input  string
+		output string
+		err    string
+	}{
+		{"", "", "open : no such file or directory"},
+		{"random.json", "", "open random.json: no such file or directory"},
+		{"config.json", "testnamespace", ""},
+		{"default.json", "default", ""},
+	}
+
+	for _, tc := range testCases {
+		namespace, err := username(tc.input)
+		if err != nil {
+			if err.Error() != tc.err {
+				t.Errorf("Error does not match! Expecting [%s], got [%s]", tc.err, err.Error())
+			}
+		}
+		if namespace != tc.output {
+			t.Errorf("Namespace does not match! Expecting %s, got %s", tc.output, namespace)
+		}
+	}
+
+	os.Remove("config.json")
+	os.Remove("default.json")
+
+}
+func TestNewClient(t *testing.T) {
+}
