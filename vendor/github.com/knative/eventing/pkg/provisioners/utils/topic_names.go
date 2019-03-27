b492a2e57bbf640379f5e2010f2b4794ca692156
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-03-27 18:24:20 +0600

    Clean up vendor

--- vendor/github.com/knative/eventing/pkg/provisioners/utils/topic_names.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import (
-	"fmt"
-)
-
-func TopicName(channelSeparator, channelNamespace, channelName string) string {
-	return fmt.Sprintf("knative-eventing-channel%s%s%s%s", channelSeparator, channelNamespace, channelSeparator, channelName)
-}
--- vendor/github.com/knative/serving/community/README.md
+++
@@ -1,5 +0,0 @@
-# Knative Community
-
-For community information, see
-[Knative Community](https://github.com/knative/docs/tree/master/community) in
-the [Docs](https://github.com/knative/docs) repo.
