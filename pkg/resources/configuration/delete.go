7e29abd88913d02198da21e46e2911450f27c098
tzununbekov <t.zununbekov@gmail.com>
2020-11-24 19:00:28 +0600

    Service deletion API version fix

--- pkg/resources/configuration/delete.go
+++ pkg/resources/configuration/delete.go
@@ -20,5 +20,5 @@ import (
 )
 
 func (c *Configuration) Delete(clientset *client.ConfigSet) error {
-	return clientset.Serving.ServingV1alpha1().Configurations(c.Namespace).Delete(c.Name, &metav1.DeleteOptions{})
+	return clientset.Serving.ServingV1().Configurations(c.Namespace).Delete(c.Name, &metav1.DeleteOptions{})
 }
